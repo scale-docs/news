905d5b82bbe9a54b70138bdbe509a09558b8d99f
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2025-09-28 23:41:31 +0000

    Deploying to gh-pages from @ ftm1337/news@1f2efb90a0a9aefe2eadefb5411f1f4ee7fece0b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ftm1337/a",
+    "data-repo": "ftm1337/news",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
